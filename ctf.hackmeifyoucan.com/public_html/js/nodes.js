e8051ff0711c32d64573e236fd2c34b3be326e01
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2024-11-29 13:54:40 +0100

    Revert "Merge pull request #4 from j0rd1s3rr4n0/Docker"
    This reverts commit 17e00bdd79b76855a08bb876371d7842cbe7c815, reversing
    changes made to 6a35f3220a3ee2c6068437170ddd5f4bda3e01dd.

--- ctf.hackmeifyoucan.com/public_html/js/nodes.js
+++ ctf.hackmeifyoucan.com/public_html/js/nodes.js
@@ -180,14 +180,13 @@ var NodesJs = (function (parameters) {
 
                 _node[3] = [];
 
-                for (var _node2_i = 0; _node2_i < t_NodesJs.nodes.length; _node2_i++) {
-                    var _node2 = t_NodesJs.nodes[_node2_i];
+                t_NodesJs.nodes.forEach(function (_node2, _node2_i) {
                     if (_node_i == _node2_i) {
-                        continue;
+                        return true;
                     }
 
                     if (_node[3].indexOf(_node2_i) > -1) {
-                        continue;
+                        return true;
                     }
 
                     var dx = Math.abs(_node[0] - _node2[0]);
